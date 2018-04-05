787fbdea045acaada13e89e3f3222aa129769c0c
Dan Haywood <dan@haywood-associates.co.uk>
2018-04-05 16:02:56 +0100

    adds lombok are removes getters and setters for Owner name and notes

--- myapp/src/main/java/domainapp/dom/impl/Owner.java
+++ myapp/src/main/java/domainapp/dom/impl/Owner.java
@@ -40,6 +40,9 @@ import org.apache.isis.applib.services.message.MessageService;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.apache.isis.applib.services.title.TitleService;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE, schema = "myapp" )
 @javax.jdo.annotations.DatastoreIdentity(strategy = IdGeneratorStrategy.IDENTITY, column = "id")
 @javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column ="version")
@@ -55,15 +58,13 @@ public class Owner implements Comparable<Owner> {
     @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
     @Property(editing = Editing.DISABLED)
     @Title(prepend = "Object: ")
+    @Getter @Setter
     private String name;
-    public String getName() { return name; }
-    public void setName(final String name) { this.name = name; }
 
     @javax.jdo.annotations.Column(allowsNull = "true", length = 4000)
     @Property(editing = Editing.ENABLED)
+    @Getter @Setter
     private String notes;
-    public String getNotes() { return notes; }
-    public void setNotes(final String notes) { this.notes = notes; }
 
 
     @Action(semantics = SemanticsOf.IDEMPOTENT, command = CommandReification.ENABLED, publishing = Publishing.ENABLED)
