f5a2eb233c5fe1ccbe50e68d0d3647e64e2549fb
Dan Haywood <dan@haywood-associates.co.uk>
2018-04-09 08:22:04 +0100

    renames app from "hello world" to "pet clinic"

--- myapp/src/main/java/domainapp/application/PetClinicAppManifest.java
+++ myapp/src/main/java/domainapp/application/PetClinicAppManifest.java
@@ -16,10 +16,24 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package domainapp.dom;
+package domainapp.application;
 
-import org.apache.isis.applib.ModuleAbstract;
+import org.apache.isis.applib.AppManifestAbstract2;
 
-public class HelloWorldModule extends ModuleAbstract {
+import domainapp.dom.PetClinicModule;
+
+/**
+ * Bootstrap the application.
+ */
+public class PetClinicAppManifest extends AppManifestAbstract2 {
+
+    public static final Builder BUILDER = Builder
+            .forModule(new PetClinicModule())
+            .withConfigurationPropertiesFile(PetClinicAppManifest.class, "isis-non-changing.properties")
+            .withAuthMechanism("shiro");
+
+    public PetClinicAppManifest() {
+        super(BUILDER);
+    }
 
 }
