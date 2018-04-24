8dc1158d9b1327afac44dc187262a65de71559d4
Dan Haywood <dan@haywood-associates.co.uk>
2018-04-24 21:04:24 +0200

    updates RecreateOwners to create pets for owners also

--- myapp/src/main/java/domainapp/dom/impl/RecreateOwners.java
+++ myapp/src/main/java/domainapp/dom/impl/RecreateOwners.java
@@ -5,26 +5,55 @@ import javax.inject.Inject;
 import org.apache.isis.applib.fixturescripts.FixtureScript;
 import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 
+import lombok.Data;
+
 public class RecreateOwners extends FixtureScript {
 
     public RecreateOwners() {
         super(null, null, Discoverability.DISCOVERABLE);
     }
 
+    @Data
+    static class PetData {
+        private final String name;
+        private final PetSpecies petSpecies;
+    }
+
     @Override
     protected void execute(final ExecutionContext ec) {
 
+        isisJdoSupport.deleteAll(Pet.class);
         isisJdoSupport.deleteAll(Owner.class);
 
         ec.addResult(this,
-                this.owners.create("Smith", "John", null));
+                createOwner("Smith", "John", null,
+                        new PetData("Rover", PetSpecies.Dog))
+        );
         ec.addResult(this,
-                this.owners.create("Jones", "Mary", "+353 1 555 1234"));
+                createOwner("Jones", "Mary", "+353 1 555 1234",
+                        new PetData("Tiddles", PetSpecies.Cat),
+                        new PetData("Harry", PetSpecies.Budgerigar)
+                ));
         ec.addResult(this,
-                this.owners.create("Hughes", "Fred", "07777 987654"));
+                createOwner("Hughes", "Fred", "07777 987654",
+                        new PetData("Jemima", PetSpecies.Hamster)
+                ));
+    }
+
+    private Owner createOwner(
+            final String lastName,
+            final String firstName,
+            final String phoneNumber,
+            final PetData... pets) {
+        Owner owner = this.owners.create(lastName, firstName, phoneNumber);
+        for (PetData pet : pets) {
+            owner.newPet(pet.name, pet.petSpecies);
+        }
+        return owner;
     }
 
     @Inject
     Owners owners;
-    @Inject IsisJdoSupport isisJdoSupport;
+    @Inject
+    IsisJdoSupport isisJdoSupport;
 }
