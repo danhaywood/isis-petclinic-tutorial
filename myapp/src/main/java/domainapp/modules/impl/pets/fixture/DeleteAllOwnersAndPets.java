85f1f9f10c729f64f6e8681d657f18567603671f
danhaywood <dan@haywood-associates.co.uk>
2018-06-07 19:04:26 +0100

    updates fixture - delete visits also

--- myapp/src/main/java/domainapp/modules/impl/pets/fixture/DeleteAllOwnersAndPets.java
+++ myapp/src/main/java/domainapp/modules/impl/pets/fixture/DeleteAllOwnersAndPets.java
@@ -4,10 +4,12 @@ import org.apache.isis.applib.fixturescripts.teardown.TeardownFixtureAbstract2;
 
 import domainapp.modules.impl.pets.dom.Owner;
 import domainapp.modules.impl.pets.dom.Pet;
+import domainapp.modules.impl.visits.dom.Visit;
 
 public class DeleteAllOwnersAndPets extends TeardownFixtureAbstract2 {
     @Override
     protected void execute(final ExecutionContext ec) {
+        deleteFrom(Visit.class);
         deleteFrom(Pet.class);
         deleteFrom(Owner.class);
     }
